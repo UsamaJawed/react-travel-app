e28229ca0fb64518941f97ef49b6331a240f58af
Usama Jawed <usamajawed573@gmail.com>
2024-01-02 18:45:39 +0500

    Made some modification to the layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,12 +27,32 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
 }
 function Stats() {
   return (
     <footer className="stats">
       <em>👜You have X items in your list, and you already packed X (X%)</em>
     </footer>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
