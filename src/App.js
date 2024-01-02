bd47b2ee745c3ecc69b63a5f06c402fd410ebbee
Usama Jawed <usamajawed573@gmail.com>
2024-01-03 02:12:13 +0500

    Added form element

--- src/App.js
+++ src/App.js
@@ -20,10 +20,24 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -31,7 +45,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => {
-          return <Item item={item} />;
+          return <Item item={item} key={item.id} />;
         })}
       </ul>
     </div>
