8efcc4204f7508a55d414b08c5ec0a097e740a12
Usama Jawed <usamajawed573@gmail.com>
2024-01-07 01:28:27 +0500

    Used newItem

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function Form() {
 
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
 
     setDescription("");
     setQuantity(1);
